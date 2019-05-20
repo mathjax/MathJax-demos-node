57586a5d3f4660acc0a8adc5df21dbad74ccf857
Davide P. Cervone <dpvc@union.edu>
2019-05-20 15:22:15 -0400

    Update the example code for formatting and to remove unneeded code

--- speech/action.js
+++ speech/action.js
@@ -8,7 +8,7 @@ const STATE = require('mathjax3/mathjax3/core/MathItem.js').STATE;
 function removeSemanticData(math) {
     math.root.walkTree(node => {
         const attributes = node.attributes.getAllAttributes();
-        delete attributes.xmlns;
+        delete attributes.xmlns;    // some internal nodes get this attribute for some reason
         for (const name of Object.keys(attributes)) {
             if (name.substr(0, 14) === 'data-semantic-' && name !== 'data-semantic-speech') {
                 delete attributes[name];
@@ -17,6 +17,12 @@ function removeSemanticData(math) {
     });
 }
 
+//
+//  The renderActions needed to remove the data-semantic-attributes.
+//    STATE.ENRICHED is the priority of the enrichment, so this will rung just after enrichment.
+//    The first function is the one for when the document's render() method is called.
+//    The second is for when a MathItem's render(), rerender() or convert() method is called.
+//
 exports.speechAction = {
     simplfy: [
         STATE.ENRICHED + 1,
