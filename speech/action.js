acb2bbfb65f1c94ca27163ee05247e50f0aa8176
zorkow <volker.sorge@gmail.com>
2022-05-17 18:20:35 +0200

    Updates all demo files.

--- speech/action.js
+++ speech/action.js
@@ -1,6 +1,6 @@
 require('mathjax-full/js/util/asyncLoad/node.js');
+// This is loaded to import `STATE.ENRICHED` is set.
 require('mathjax-full/js/a11y/semantic-enrich.js');
-require('mathjax-full/js/a11y/sre-node.js');
 const {STATE} = require('mathjax-full/js/core/MathItem.js');
 
 //
@@ -19,22 +19,61 @@ function removeSemanticData(math) {
 }
 
 
-const sreDefault = {
-    domain: 'mathspeak',
-    style: 'default'
+//
+// Moves a speech element into the description element for SVG output.
+//
+// Note, that here we cannot walk the source tree, as we alterations are to be
+// done on the typeset element, if it is a SVG node.
+//
+function speechToDescription(math) {
+  const root = math.typesetRoot;
+  const adaptor = math.adaptor;
+  const svg = adaptor.childNodes(root)[0];
+  if (!svg || adaptor.kind(svg) !== 'svg') return;
+  const children = [svg];
+  while (children.length) {
+    let child = children.shift();
+    if (adaptor.kind(child) === '#text') continue;
+    if (adaptor.hasAttribute(child, 'data-semantic-speech')) {
+      let desc = adaptor.create('desc');
+      let text = adaptor.text(adaptor.getAttribute(child, 'data-semantic-speech'));
+      adaptor.append(desc, text);
+      adaptor.append(child, desc);
+      adaptor.removeAttribute(child, 'data-semantic-speech');
+    }
+    children.push(...adaptor.childNodes(child));
+  }
+}
+
+//
+// Configures SRE from key value pairs by populating MathJax's config options.
+//
+exports.dataPairs = function(data) {
+  const config = {};
+  if (data) {
+    for (let i = 0, key; key = data[i]; i++) {
+      let value = data[++i];
+      config[key] = value || false;
+    }
+  }
+  return config;
 };
 
-
-// Configures SRE from key value pairs.
 exports.sreconfig = function(data) {
-    let config = {};
-    if (data) {
-        for (let i = 0, key; key = data[i]; i++) {
-            let value = data[++i];
-            config[key] = value || false;
-        }
+  const config = exports.dataPairs(data);
+  if (data) {
+    for (let i = 0, key; key = data[i]; i++) {
+      let value = data[++i];
+      config[key] = value || false;
     }
-    sre.setupEngine(Object.assign({}, sreDefault, config));
+  }
+  if (!MathJax.config.options) {
+    MathJax.config.options = {};
+  }
+  if (!MathJax.config.options.sre) {
+    MathJax.config.options.sre = {};
+  }
+  Object.assign(MathJax.config.options.sre, config);
 };
 
 
@@ -45,7 +84,7 @@ exports.sreconfig = function(data) {
 //    The second is for when a MathItem's render(), rerender() or convert() method is called.
 //
 exports.speechAction = {
-  simplfy: [
+  simplify: [
     STATE.ENRICHED + 1,
     (doc) => {
       for (const math of doc.math) {
@@ -55,5 +94,17 @@ exports.speechAction = {
     (math, doc) => {
       removeSemanticData(math);
     }
+  ],
+  describe: [
+    1000,
+    (doc) => {
+      for (const math of doc.math) {
+        speechToDescription(math);
+      }
+    },
+    (math, doc) => {
+      speechToDescription(math);
+    }
   ]
+
 };
